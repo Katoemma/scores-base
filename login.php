e34001cea5d7c9761e6cce3dd6b2414b462f3e88
elias-avenger <eli0@outlook.com>
2023-04-22 20:31:46 +0300

    Adding a login file to implement login funtionality

---
+++ login.php
@@ -0,0 +1,20 @@
+<?php
+    //including a file that connects to the server and the database
+    include "connect.php";
+
+    //getting values from the HTML form
+    $email = $_POST['email'];
+    $pass = $_POST['password'];
+    
+    //fetching data from the db
+    $query = mysqli_query($con, 
+        "SELECT * FROM admin WHERE email = '$email' AND password = '$pass'"
+    );
+    $rows = mysqli_num_rows($query);
+    if($rows == 1){
+        header("location: dashboard.php");
+    }
+    else{
+        header("location: index.php");
+    }
+?>
